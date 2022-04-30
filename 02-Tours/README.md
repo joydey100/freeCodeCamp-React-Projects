dd0ee1b57fb6b679226c26bb1c802d8c3661092d
Joy Dey <joydey.bd.2002@gmail.com>
2022-04-30 23:48:48 +0600

    second done

--- 02-Tours/README.md
+++ 02-Tours/README.md
@@ -1,1 +1,7 @@
-## React Projects Starter APP
+## Tours
+
+This is a Tours project, Here used api for fetching data with useEffect. I practice it with freeCodeCamp 15 projects with react by John Smilga.
+
+Live Link - [Tours](https://birthday-reminder-project.netlify.app/)
+
+![alt text](https://raw.githubusercontent.com/joydey100/freeCodeCamp-React-Projects/main/01-Birthday%20Reminder/screenshot.png)
--- 02-Tours/src/App.js
+++ 02-Tours/src/App.js
@@ -1,11 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
-function App() {
-  return <h2>Tours Project Setup</h2>
-}
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
-export default App
+//URL for fetching tour data
+
+const url = "https://course-api.com/react-tours-project";
+
+const App = () => {
+  // All States
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  // fetchind Data
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  // After initial page render, tours will be showed
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  // if the loading is true, then i will show loading component
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  // if there is no tour left, then it will show there is no tour left and a button which will load again fetch the tours
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  // Remove function
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+};
+
+export default App;
